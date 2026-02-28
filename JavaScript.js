16bd07df227b18cab77d82f642b67798e6c89c24
Alisha Mughal <alisha.mughal.2006@gmail.com>
2026-02-28 08:33:44 -0800

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,58 @@
+// Var
+var name = "Alisha";
+var name = "Sara";   // re-declared (allowed)
+name = "Areeba";     // re-assigned (allowed)
+
+console.log(name);
+
+
+// Let
+let city = "Lahore";
+city = "Faisalabad";   // allowed
+
+// let city = "Karachi"; ❌ Error (cannot re-declare)
+console.log(city);
+
+// Const
+const pi = 3.14;
+// pi = 3.15; ❌ Error
+
+console.log(pi);
+
+
+// Data Types (Basics)
+//String
+let username = "Alisha";
+console.log(username);
+
+// Numbers
+let age = 19;
+let price = 99.99;
+
+console.log(age);
+console.log(price);
+
+// Boolean
+let isStudent = true;
+console.log(isStudent);
+
+// Undefined
+let country;
+console.log(country);
+
+// Null
+let data = null;
+console.log(data);
+
+
+// Input 
+let firstname = prompt("Enter your firstname:");
+console.log(firstname);
+
+// Converting input to number
+let num1 = prompt("Enter first number:");
+let num2 = prompt("Enter second number:");
+
+let sum = num1 + num2;
+console.log(sum);
+
